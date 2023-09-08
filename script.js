579bcb683596a2f7ccb860510e4df81b8b0c3cb6
qrex19 <nishanttjhaa25@gmail.com>
2023-09-09 03:08:22 +0530

    some basic functionality added

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-// Math.floor(Math.random() * arr.length)
-
 const xImage = '<img src="./images/x.png" alt="">';
 const oImage = '<img src="./images/o.png" alt="">';
-
-let boardStatus = true; //* default mode
 
 const buttonObject = (number) => {
   const index = document.querySelector(`.${number}`);
@@ -33,11 +29,27 @@ for (let i = 0; i < board.length; i++) {
 //* to generate a random number between 0 and 8 (both inclusive)
 
 function returnIndex(arr, target) {
-    for(let a = 0; a < arr.length; a++) {
-        if(arr[a] == target) {
-            return a;
-        }
+  for (let a = 0; a < arr.length; a++) {
+    if (arr[a] == target) {
+      return a;
     }
+  }
+}
+
+function usersTurn(object, i) {
+  if (object.index.innerHTML != oImage && object.index.innerHTML != xImage) {
+    object.index.innerHTML = xImage;
+    optionsArr.splice(returnIndex(optionsArr, i), 1);
+  }
+}
+
+function computersTurn() {
+  let rand = Math.floor(Math.random() * optionsArr.length); //returns a random index from the array
+  let num = optionsArr[rand];
+
+  board[num].index.innerHTML = oImage;
+  optionsArr.splice(returnIndex(optionsArr, num), 1);
+  console.log(optionsArr);
 }
 
 let optionsArr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
@@ -47,22 +59,9 @@ function game() {
   for (let i = 0; i < board.length; i++) {
     board[i].index.addEventListener("click", () => {
 
-        //* logic
-
-        if(board[i].index.innerHTML != oImage && board[i].index.innerHTML != xImage) {
-            board[i].index.innerHTML = xImage
-            optionsArr.splice(returnIndex(optionsArr, i), 1);
-        }
-
-        //* computer's turn
-        let rand = Math.floor(Math.random() * optionsArr.length); //returns a random index from the array
-        let num = optionsArr[rand];
-
-        board[num].index.innerHTML = oImage;
-        optionsArr.splice(returnIndex(optionsArr, num), 1);
-        console.log(optionsArr);
-
-
+      usersTurn(board[i], i);
+      computersTurn();
+      
     });
   }
 }
